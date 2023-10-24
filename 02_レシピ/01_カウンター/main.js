f378da4c5d23f18d4cc7fb1315e2762bd1470982
tomoka115 <tmkhreuph@gmail.com>
2023-10-24 11:43:41 +0900

    [カウンター]を学習しました。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,10 +1,27 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+const doubleButton = document.getElementById("double-button")
+let count = 0
 
 // ボタンをクリックしたときの処理を登録
 plusButton.onclick = function () {
   // count を更新
   count += 1
+  // count を表示
+  display.textContent = count
+}
+
+minusButton.onclick = function () {
+  // count を更新
+  count -= 1
+  // count を表示
+  display.textContent = count
+}
+
+doubleButton.onclick = function () {
+  // count を更新
+  count *= 2
   // count を表示
   display.textContent = count
 }
